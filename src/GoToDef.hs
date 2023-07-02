bc6e3a9ad438f0e21bbcc3d25585c2746c535e4a
Mistral Contrastin <madgenhetic@gmail.com>
2023-07-02 14:52:25 +0100

    Make lastDef keep multiple defs

--- src/GoToDef.hs
+++ src/GoToDef.hs
@@ -8,7 +8,9 @@ import           Prelude hiding (id)
 
 type GoToDef id = M.Map id [id]
 
-type Env id = (M.Map String id, GoToDef id)
+type LastOcc id = M.Map String [id]
+
+type Env id = (LastOcc id, GoToDef id)
 
 data Mode = First
           | Last
@@ -19,41 +21,40 @@ goToDef First = goToDefSimple (\_ y -> y)
 goToDef Last = goToDefSimple const
 
 goToDefSimple
-  :: forall id. Ord id => (id -> id -> id) -> Program id -> GoToDef id
+  :: forall id. Ord id => ([id] -> [id] -> [id]) -> Program id -> GoToDef id
 goToDefSimple choose pr = snd $ goBlock (M.empty, M.empty) pr
   where
     goBlock :: Env id -> Program id -> Env id
     goBlock = foldl goSt
 
     goSt :: Env id -> Statement id -> Env id
-    goSt env@(lastOcc, _) (Assignment var@(Variable id str) expr) =
-      (lastOcc', goToDefTbl')
+    goSt env@(lastDef, _) (Assignment var@(Variable id str) expr) =
+      (lastDef', goToDefTbl')
       where
-        lastOcc' = M.insertWith choose str id lastOcc
+        lastDef' = M.insertWith choose str [id] lastDef
 
-        env' = (lastOcc', goExpr env expr)
+        env' = (lastDef', goExpr env expr)
 
         goToDefTbl' = goVar env' var
-    goSt env@(lastOcc, _) (Ite e th el) =
-      (M.unionWith choose lastOccEl lastOccTh, goToDefTbl'')
+    goSt env@(lastDef, _) (Ite e th el) =
+      (M.unionWith choose lastDefEl lastDefTh, goToDefTbl'')
       where
         goToDefTbl = goExpr env e
 
-        (lastOccTh, goToDefTbl') = foldl goSt (lastOcc, goToDefTbl) th
+        (lastDefTh, goToDefTbl') = foldl goSt (lastDef, goToDefTbl) th
 
-        (lastOccEl, goToDefTbl'') = foldl goSt (lastOcc, goToDefTbl') el
-    goSt env@(lastOcc, _) (While e body) =
-      foldl goSt (lastOcc, goExpr env e) body
+        (lastDefEl, goToDefTbl'') = foldl goSt (lastDef, goToDefTbl') el
+    goSt env@(lastDef, _) (While e body) =
+      foldl goSt (lastDef, goExpr env e) body
 
     goExpr :: Env id -> Expression id -> GoToDef id
     goExpr (_, goToDefTbl) EConst {} = goToDefTbl
     goExpr env (EVar var) = goVar env var
-    goExpr env@(lastOcc, _) (EPlus e1 e2) = goExpr (lastOcc, goExpr env e1) e2
+    goExpr env@(lastDef, _) (EPlus e1 e2) = goExpr (lastDef, goExpr env e1) e2
 
-    goVar (lastOcc, goToDefTbl) (Variable id str) = maybe
-      goToDefTbl
-      (\v -> M.insert id [v] goToDefTbl)
-      (M.lookup str lastOcc)
+    goVar :: Env id -> Variable id -> GoToDef id
+    goVar (lastDef, goToDefTbl) (Variable id str) =
+      maybe goToDefTbl (\v -> M.insert id v goToDefTbl) (M.lookup str lastDef)
 
 tabulate :: GoToDef id -> [(id, [id])]
 tabulate = M.assocs
